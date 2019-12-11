df4c92c295b38eacd0210af9b008db01682f4923
stefandrea <50913331+stefandrea@users.noreply.github.com>
2019-12-11 11:44:12 -0500

    version 1.0

---
+++ bd.sql
@@ -0,0 +1,14 @@
+create table creador(
+    id_creador int,
+    edad int,
+    nombre varchar(200),
+    experiencia varchar(200) 
+);
+
+insert into creador (id_creador,edad,nombre,experiencia) values
+(2, 23, 'Ice', 'origin'),
+(4, 25, 'Andrea', 'capcom'),
+(5, 28, 'Shawn', 'nintendo'),
+(8, 22, 'David', 'SEGA'),
+(1, 30, 'Lana', 'koei'),
+(7, 26, 'Harry', 'Sony');
