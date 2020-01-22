58102b10c71ab606ac8ac1024f9516d27773d516
stefandrea <50913331+stefandrea@users.noreply.github.com>
2020-01-22 11:46:20 -0500

    1.2

--- bd.sql
+++ bd.sql
@@ -1,14 +1,51 @@
 create table creador(
     id_creador int,
     edad int,
-    nombre varchar(200),
-    experiencia varchar(200) 
+    nombre varchar(20),
+    experiencia varchar(50),
+    numero_de_serie varchar(20);
+
+    primary key(id_creador),
+    foreign key(numero_de_serie) references juego.numero_de_serie 
 );
 
-insert into creador (id_creador,edad,nombre,experiencia) values
-(2, 23, 'Ice', 'origin'),
-(4, 25, 'Andrea', 'capcom'),
-(5, 28, 'Shawn', 'nintendo'),
-(8, 22, 'David', 'SEGA'),
-(1, 30, 'Lana', 'koei'),
-(7, 26, 'Harry', 'Sony');
+create table juego(
+    numero_de_serie varchar(20),
+    dificultad varchar(5),
+    nombre_creador varchar(20),
+    nivel int,
+    id_creador varchar(20),
+    id_tematica varchar(20),
+
+    primary key(numero_de_serie),
+    foreign key(id_creador) references creador.id_creador
+    foreign key(id_tematica) references tematica.id_tematica
+);
+
+create table tematica(
+    id_tematica int,
+    interfaz varchar(20),
+    arcade varchar(20),
+    fantasia varchar(20),
+
+    primary key(id_tematica),
+);
+
+create table consola(
+    generaciones varchar(20),
+    xbox360 varchar(20),
+    playstation varchar(20),
+
+    primary key(generaciones),
+)
+
+create table juega(
+    generaciones varchar(20),
+    numero_de_serie varchar(20),
+    fecha date(),
+
+    foreign key(generaciones) references consola.generaciones
+    foreign key(numero_de_serie) references juego.numero_de_serie
+
+)
+
