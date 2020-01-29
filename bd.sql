cb9637d2834b2d9212e6f508c3847e232d299560
stefandrea <50913331+stefandrea@users.noreply.github.com>
2020-01-29 11:38:41 -0500

    version 1.0

--- bd.sql
+++ bd.sql
@@ -3,10 +3,7 @@ create table creador(
     edad int,
     nombre varchar(20),
     experiencia varchar(50),
-    numero_de_serie varchar(20);
-
-    primary key(id_creador),
-    foreign key(numero_de_serie) references juego.numero_de_serie 
+    numero_de_serie varchar(20)  
 );
 
 create table juego(
@@ -15,37 +12,43 @@ create table juego(
     nombre_creador varchar(20),
     nivel int,
     id_creador varchar(20),
-    id_tematica varchar(20),
-
-    primary key(numero_de_serie),
-    foreign key(id_creador) references creador.id_creador
-    foreign key(id_tematica) references tematica.id_tematica
+    id_tematica varchar(20)
 );
 
 create table tematica(
     id_tematica int,
     interfaz varchar(20),
     arcade varchar(20),
-    fantasia varchar(20),
-
-    primary key(id_tematica),
+    fantasia varchar(20)
 );
 
 create table consola(
     generaciones varchar(20),
     xbox360 varchar(20),
-    playstation varchar(20),
-
-    primary key(generaciones),
-)
+    playstation varchar(20)   
+);
 
 create table juega(
     generaciones varchar(20),
     numero_de_serie varchar(20),
-    fecha date(),
+    fecha date()
+);
 
-    foreign key(generaciones) references consola.generaciones
-    foreign key(numero_de_serie) references juego.numero_de_serie
+alter table 
+    add constraint creador_pk1 primary key(id_creador),
+    add constraint creador_fk1 foreign key(numero_de_serie) references juego(numero_de_serie);
 
-)
+alter table
+    add constraint juego_pk1 primary key(numero_de_serie),
+    add constraint juego_fk1 foreign key(id_creador) references creador(id_creador),
+    add constraint juego_fk2 foreign key(id_tematica) references tematica(id_tematica);
 
+alter table
+    add constraint tematica_pk1 primary key(id_tematica);
+
+alter table
+    add constraint consola_pk1 primary key(generaciones);
+
+alter table
+    add constraint juega_fk1 foreign key(generaciones) references consola(generaciones),
+    add constraint juega_fk2 foreign key(numero_de_serie) references juego(numero_de_serie);
